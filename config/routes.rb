268661cfcb161dc674f01edbfe1a5252151d3c4b
Eoin Lafferty <eoinlafferty@gmail.com>
2020-06-22 16:18:23 +0100

    installed user devises and added initial routes following setup from lecture

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  devise_for :users
   root to: 'pages#home'
+  resources :photographers do
+    resources :bookings, only: [:create, :new, :show]
+    resources :photos, only: [:create, :new, :show, :destroy]
+  end
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
