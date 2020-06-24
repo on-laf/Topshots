12b32800ac63e76bfe996798c10bdead2b71ac7d
Eoin Lafferty <eoinlafferty@gmail.com>
2020-06-24 22:08:57 +0100

    added authorization and authentication to photographs controller

--- app/controllers/photographs_controller.rb
+++ app/controllers/photographs_controller.rb
@@ -1,12 +1,16 @@
 class PhotographsController < ApplicationController
+  skip_before_action :authenticate_user!, only: [:index, :show]
+
   def new
     @photograph = Photograph.new
+    authorize @photograph
   end
 
   def create
     @photograph = Photograph.new(photograph_params)
     @photographer = Photographer.find(params[:photographer_id])
     @photograph.photographer = @photographer
+    authorize @photograph
     if @photograph.save
       redirect_to photographer_path(@photographer)
     else
@@ -16,15 +20,18 @@ class PhotographsController < ApplicationController
 
   def index
     @photographs = policy_scope(Photograph)
+    authorize @photographs
   end
 
   def show
     @photograph = Photograph.find(params[:id])
+    authorize @photograph
   end
 
   def destroy
     @photograph = Photograph.find(params[:id])
     @photograph.destroy
+    authorize @photograph
     redirect_to photographer_path(@photographer)
   end
 
